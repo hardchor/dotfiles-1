76c883015fbf0c4f1efc6dbdbef8654ea85df2f1
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2019-03-03 01:26:47 +0100

    rename ZSH var to get ready for oh-my-zsh

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,3 @@
-export PATH="$EC2_HOME/bin:$HOME/.npm-packages/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$EC2_HOME/bin:$HOME/.npm-packages/bin:./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
